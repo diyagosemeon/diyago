bc09c572d1a75e20cff9a744e72a24d6910a7aa7
diyagosemeon <42169900+diyagosemeon@users.noreply.github.com>
2018-08-07 14:20:54 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class sample
+{
+public static void main(String arg[])
+{
+Scanner s=new Scanner(System.in);
+int i=s.nextInt();
+if(i<0)
+{
+System.out.println("NEGATIVE");
+}
+else if(i==0)
+{
+System.out.println("ZERO");
+}
+else
+{
+System.out.println("POSITIVE");
+}
+}
+}
